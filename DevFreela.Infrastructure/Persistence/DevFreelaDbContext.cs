1e01872ffb6e74d08b5c5ae8babdee61378b54d4
Mend1s <andremendesskt99@gmail.com>
2022-05-27 19:33:47 -0300

    Criando ClassLibrary de Infraestrutura e DbContext

---
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DevFreela.Core.Entities;
+
+namespace DevFreela.Infrastructure.Persistence
+{
+    public class DevFreelaDbContext
+    {
+        public List<Project> Projects { get; set; }
+        public List<User> Users { get; set; }
+        public List<Skill> Skills { get; set; }
+
+    }
+}
