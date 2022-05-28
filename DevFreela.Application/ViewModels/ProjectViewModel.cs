fefa1192ab0cf0fbe89a1fd4a62b0ab2939e2f0c
Mend1s <andremendesskt99@gmail.com>
2022-05-28 00:34:39 -0300

    Implementando SkillService e ISkillService

--- DevFreela.Application/ViewModels/ProjectViewModel.cs
+++ DevFreela.Application/ViewModels/ProjectViewModel.cs
@@ -2,12 +2,14 @@
 {
     public class ProjectViewModel
     {
-        public ProjectViewModel(string title, DateTime createAt)
+        public ProjectViewModel(int id, string title, DateTime createAt)
         {
+            Id = id;
             Title = title;
             CreateAt = createAt;
         }
 
+        public int Id { get; private set; }
         public string Title { get; private set; }
         public DateTime CreateAt { get; private set; }
     }
