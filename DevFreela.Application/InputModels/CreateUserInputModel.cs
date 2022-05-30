dfe9405cf3245c2723dd5185c30c2ed27730e558
Mend1s <andremendesskt99@gmail.com>
2022-05-29 22:44:00 -0300

    Add IUserService

---
+++ DevFreela.Application/InputModels/CreateUserInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.InputModels
+{
+    public class CreateUserInputModel
+    {
+        public string Name { get; private set; }
+        public string Password { get; private set; }
+        public string Email { get; private set; }
+    }
+}
---
+++ DevFreela.Application/Services/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using DevFreela.Application.InputModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Interfaces
+{
+    public interface IUserService
+    {
+        CreateUserInputModel GetById(int id);
+    }
+}
