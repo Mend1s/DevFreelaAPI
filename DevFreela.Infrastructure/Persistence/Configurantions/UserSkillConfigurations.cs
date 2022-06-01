a06de5126b81d5a03e97e11272d1ca63b7179855
Mend1s <andremendesskt99@gmail.com>
2022-06-01 13:09:11 -0300

    Refatorando mapeando das entidades com EntityFrameworkCore

---
+++ DevFreela.Infrastructure/Persistence/Configurantions/UserSkillConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevFreela.Infrastructure.Persistence.Configurantions
+{
+    public class UserSkillConfigurations : IEntityTypeConfiguration<UserSkill>
+    {
+        public void Configure(EntityTypeBuilder<UserSkill> builder)
+        {
+            builder
+                .HasKey(s => s.Id);
+        }
+    }
+}
