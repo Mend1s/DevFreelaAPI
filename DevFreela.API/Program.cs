00ad9ad5d8425d670718688a9a4a3a9ce6d4a3d2
Mend1s <andremendesskt99@gmail.com>
2022-06-01 13:42:26 -0300

    Configurando ConnectionStrings e criando Migrations

--- DevFreela.API/Program.cs
+++ DevFreela.API/Program.cs
@@ -2,6 +2,7 @@ using DevFreela.API.Models;
 using DevFreela.Application.Services.Implementations;
 using DevFreela.Application.Services.Interfaces;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +14,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.Configure<OpeningTimeOption>
     (builder.Configuration.GetSection("OpeningTime"));
-builder.Services.AddSingleton<ExampleClass>(e => new ExampleClass { Name = "Initial Stage" });
 
-builder.Services.AddSingleton<DevFreelaDbContext>();
+builder.Services.AddDbContext<DevFreelaDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevFreelaCs"));
+});
 builder.Services.AddScoped<IProjectService, ProjectService>();
 
 var app = builder.Build();
