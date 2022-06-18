71e3752d738b6acfc145001e1dbfc3783e48bf08
Mend1s <andremendesskt99@gmail.com>
2022-06-18 00:32:39 -0300

    Refatorando Querie User

--- DevFreela.Application/Queries/GetUser/GetUserQueryHandler.cs
+++ DevFreela.Application/Queries/GetUser/GetUserQueryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using DevFreela.Application.ViewModels;
+using DevFreela.Core.Repositories;
 using DevFreela.Infrastructure.Persistence;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -9,15 +10,15 @@ namespace DevFreela.Application.Queries.GetUser
 {
     public class GetUserQueryHandler : IRequestHandler<GetUserQuery, UserViewModel>
     {
-        private readonly DevFreelaDbContext _dbContext;
-        public GetUserQueryHandler(DevFreelaDbContext dbContext)
+        private readonly IUserRepository _userRepository;
+        public GetUserQueryHandler(IUserRepository userRepository)
         {
-            _dbContext = dbContext;
+            _userRepository = userRepository;
         }
 
         public async Task<UserViewModel> Handle(GetUserQuery request, CancellationToken cancellationToken)
         {
-            var user = await _dbContext.Users.SingleOrDefaultAsync(u => u.Id == request.Id);
+            var user = await _userRepository.GetByIdAsync(request.Id);
 
             if (user == null)
             {
