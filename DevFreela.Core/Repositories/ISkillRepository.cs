1ad182a5c655bccdd266782451e5bd33ba69ae9c
Mend1s <andremendesskt99@gmail.com>
2022-06-20 23:16:07 -0300

    limpando usings

--- DevFreela.Core/Repositories/ISkillRepository.cs
+++ DevFreela.Core/Repositories/ISkillRepository.cs
@@ -1,9 +1,4 @@
 ﻿using DevFreela.Core.DTOs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DevFreela.Core.Repositories
 {
