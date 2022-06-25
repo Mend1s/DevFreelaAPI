f10521917cdcd6f0352139b2faf017e563ec4ddb
Mend1s <andremendesskt99@gmail.com>
2022-06-25 14:47:03 -0300

    Add UpdateProjectCommandValidator

---
+++ DevFreela.Application/Validators/UpdateProjectCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using DevFreela.Application.Commands.UpdateProject;
+using FluentValidation;
+
+namespace DevFreela.Application.Validators
+{
+    public class UpdateProjectCommandValidator : AbstractValidator<UpdateProjectCommand>
+    {
+        public UpdateProjectCommandValidator()
+        {
+            RuleFor(p => p.Description)
+                .MaximumLength(255)
+                .WithMessage("Tamanho máximo de Descriçao é de 255 caracteres.");
+
+            RuleFor(p => p.Title)
+                .MaximumLength(30)
+                .WithMessage("Tamanho máximo de Título é de 30 caracteres");
+        }
+    }
+}
