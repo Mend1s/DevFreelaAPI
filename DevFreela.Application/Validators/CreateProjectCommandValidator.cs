d639161fe9b8c56f73d70c9b759d2392b7a687c0
Mend1s <andremendesskt99@gmail.com>
2022-06-25 14:08:05 -0300

    Add FluentValidation no ProjectCommand

---
+++ DevFreela.Application/Validators/CreateProjectCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using DevFreela.Application.Commands.CreateProject;
+using FluentValidation;
+
+namespace DevFreela.Application.Validators
+{
+    public class CreateProjectCommandValidator : AbstractValidator<CreateProjectCommand>
+    {
+        public CreateProjectCommandValidator()
+        {
+            RuleFor(p => p.Description)
+                .NotEmpty()
+                .MinimumLength(30)
+                .WithMessage("Tamanho mínimo de Descrição é de 30 caracteres.")
+                .MaximumLength(255)
+                .WithMessage("Tamanho máximo de Descrição é de 255 caracteres.");
+
+            RuleFor(p => p.Title)
+                .NotEmpty()
+                .WithMessage("Título é obrigatório.")
+                .MaximumLength(30)
+                .WithMessage("Tamanho máximo de Título é de 30 caracteres.");
+        }
+    }
+}
