68b812cf0d3618c3dfbf7c59eedf1e75c06fc7c6
Mend1s <andremendesskt99@gmail.com>
2022-06-25 14:46:16 -0300

    Add CreateCommentCommandValidator

---
+++ DevFreela.Application/Validators/CreateCommentCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using DevFreela.Application.Commands.CreateComment;
+using FluentValidation;
+
+namespace DevFreela.Application.Validators
+{
+    public class CreateCommentCommandValidator : AbstractValidator<CreateCommentCommand>
+    {
+        public CreateCommentCommandValidator()
+        {
+            RuleFor(p => p.Content)
+                .MaximumLength(255)
+                .WithMessage("Tamanho máximo de Texto de Comentário é de 255 caracteres.");
+        }
+    }
+}
