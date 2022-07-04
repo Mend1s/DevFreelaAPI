5db79f9527c56f9598949fc7af60f575fd8f7125
Mend1s <andremendesskt99@gmail.com>
2022-07-04 19:19:32 -0300

    Add serviço de geração de token com JWT

---
+++ DevFreela.Core/Services/IAuthService.cs
@@ -0,0 +1,7 @@
+﻿namespace DevFreela.Core.Services
+{
+    public interface IAuthService
+    {
+        string GenerateJwtToken(string email, string role);
+    }
+}
---
+++ DevFreela.Infrastructure/Auth/AuthService.cs
@@ -0,0 +1,47 @@
+﻿using DevFreela.Core.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DevFreela.Infrastructure.Auth
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IConfiguration _configuration;
+        public AuthService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateJwtToken(string email, string role)
+        {
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+            var key = _configuration["Jwt:key"];
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim("username", email),
+                new Claim(ClaimTypes.Role, role)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                expires: DateTime.Now.AddHours(8),
+                signingCredentials: credentials,
+                claims: claims);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var stringToken = tokenHandler.WriteToken(token);
+
+            return stringToken;
+        }
+    }
+}
